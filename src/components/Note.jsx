c6b9650c173d996661b7abb90892c5cd96da36f9
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-04 15:51:00 -0500

    Note component - Added a new component that include: a btn to delete, edit and update - Update/Edit btns control inside elements according to state - When updating, an input manage the value before confirm final change.

---
+++ src/components/Note.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const Note = ({ content }) => {
+  const [note, setNote] = useState(content);
+  const [inputNote, setInputNote] = useState(note);
+  const [updating, setUpdating] = useState(false);
+
+  const editHandler = () => {
+    setUpdating((prev) => !prev);
+  };
+  const inputHandler = (e) => {
+    if (e.target.value != "") {
+      setInputNote(e.target.value);
+    }
+    return;
+  };
+  const acceptHandler = () => {
+    setNote(inputNote);
+    setUpdating((prev) => !prev);
+  };
+  return (
+    <div className="my-4 flex bg-white border-2 py-4 px-5  items-center justify-between gap-5 min-w-[40rem]">
+      {!updating ? (
+        <>
+          <p>{note}</p>
+          <div className="flex gap-2">
+            <button
+              className="bg-sky-500 py-2 px-3 rounded text-white text-center min-w-[4rem]"
+              onClick={editHandler}
+            >
+              Edit
+            </button>
+            <button className="bg-rose-500 py-2 px-3 rounded text-white text-center">
+              Delete
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <input
+            value={inputNote}
+            placeholder={note}
+            onChange={inputHandler}
+            className="w-full border border-slate-300 p-2 rounded"
+          />
+          <button
+            onClick={acceptHandler}
+            className="bg-green-500 py-2 px-3 rounded text-white text-center"
+          >
+            Update
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Note;
